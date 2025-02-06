ecc59a43ce1c8d76aeee395f088f8ff3ad6e61ed
materfe <mathieurouxdasilva2005@gmail.com>
2025-02-06 12:04:42 +0100

    adding cmake dependencies

---
+++ Dependencies.cmake
@@ -0,0 +1,31 @@
+include(cmake/CPM.cmake)
+
+# Done as a function so that updates to variables like
+# CMAKE_CXX_FLAGS don't propagate out to other
+# targets
+function(cmake_template_setup_dependencies)
+
+  # For each dependency, see if it's
+  # already been provided to us by a parent project
+
+  if(NOT TARGET fmtlib::fmtlib)
+    cpmaddpackage("gh:fmtlib/fmt#11.1.3")
+  endif()
+
+  if(NOT TARGET Catch2::Catch2WithMain)
+    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
+  endif()
+
+  if(NOT TARGET CLI11::CLI11)
+    cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
+  endif()
+
+  if(NOT TARGET ftxui::screen)
+    cpmaddpackage("gh:ArthurSonzogni/FTXUI@5.0.0")
+  endif()
+
+  if(NOT TARGET tools::tools)
+    cpmaddpackage("gh:lefticus/tools#update_build_system")
+  endif()
+
+endfunction()
